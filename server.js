91678efb8d71fc80d2226712fc5f5af632e187a7
Riaz <haris.riaz@eu.socionext.com>
2026-03-23 11:59:07 +0100

    fix host

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const addonInterface = require("./addon");
 const { serveHTTP } = require("stremio-addon-sdk");
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 10000;
 
 // ✅ IMPORTANT: enable config UI
 serveHTTP(addonInterface, {
     port: PORT,
     // this enables /configure endpoint
+    host: "0.0.0.0",
     allowOrigin: "*"
 });
